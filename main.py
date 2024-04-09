0e37f75ecaa9e3a80925bea8be640fddf6d9d40c
RichieRichMoC <137105326+RichieRichMoC@users.noreply.github.com>
2024-04-09 22:31:45 +0000

    chamge_of_main_py

--- main.py
+++ main.py
@@ -1,90 +1,100 @@
 from fastapi import FastAPI, HTTPException, Response
-import joblib
-from pydantic import BaseModel
-import pandas as pd
 import uvicorn
-
-pipeline = joblib.load('./models/rf_classifier_pipeline.joblib')
-pipeline2 = joblib.load('./models/tree_classifier_pipeline.joblib')
-pipeline3 = joblib.load('./models/logistic_classifier_pipeline.joblib')
-encoder = joblib.load('./models/label_encoder.joblib')
+from pydantic import BaseModel
+import joblib
+import pandas as pd
 
 app = FastAPI()
 
 class SepssisFeatures(BaseModel):
-  PRG: int
-  PL: int
-  PR: int
-  SK: int
-  TS: int
-  M11: float
-  BD2: float
-  Age: int
-  Insurance: int
+    PRG: int
+    PL: int
+    PR: int
+    SK: int
+    TS: int
+    M11: float
+    BD2: float
+    Age: int
+    Insurance: int
 
-
+forest_pipeline = joblib.load("./models/rf_classifier_pipeline.joblib")
+decision_tree_pipeline = joblib.load("./models/tree_classifier_pipeline.joblib")
+logistic_pipeline = joblib.load("./models/logistic_classifier_pipeline.joblib")
+encoder = joblib.load("./models/label_encoder.joblib")
 
 @app.get('/')
 def home():
-    return Response('Hello world')
+    return {"Hello World"}
 
-
+# Info route
 @app.get('/info')
 def appinfo():
-    return Response('This is the info page of this API')
+    return {
+        "title": "Welcome to the Sepsis Prediction API!",
+    }
+
+@app.post("/predict_Random_Forest_Model")
+def predict_Random_Forest(data: SepssisFeatures):
+    try:
 
 
-@app.post('/predict_Sepssis_with_rf_classifier')
-def predict_Sepssis(sepssis_features: SepssisFeatures):
 
-    try: 
-    
-        df = pd.DataFrame([sepssis_features.model_dump()])
+       df = pd.DataFrame([data.model_dump()])
 
-        prediction = pipeline.predict(df)
+       prediction = forest_pipeline.predict(df)
 
-        decoded_prediction = encoder.inverse_transform([prediction])[0]
+       prediction = int(prediction[0])
 
-        return {'prediction': decoded_prediction}
-    
+       prediction = encoder.inverse_transform([prediction])[0]
+
+       probabilities = forest_pipeline.predict_proba(df)
+
+       probabilities = probabilities.tolist()
+
+       return {'predictions': prediction, 'probabilities': probabilities}
+
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f'This is a server error {str(e)}')
+     raise HTTPException(status_code=500, detail=f'This is a server error {str(e)}')
 
 
 
-@app.post('/predict_Sepssis_with_tree_classifier')
-def predict_Sepssis(sepssis_features2: SepssisFeatures):
+@app.post("/predict_Decision_Tree_Model")
+def predict_Decision_Tree(data: SepssisFeatures):
+    try:
+       df = pd.DataFrame([data.model_dump()])
 
-    try: 
-    
-        df = pd.DataFrame([sepssis_features2.model_dump()])
+       prediction = decision_tree_pipeline.predict(df)
 
-        prediction2 = pipeline2.predict(df)
+       prediction = int(prediction[0])
 
-        decoded_prediction2 = encoder.inverse_transform([prediction2])[0]
+       prediction = encoder.inverse_transform([prediction])[0]
 
-        return {'prediction': decoded_prediction2}
-    
+       probabilities = decision_tree_pipeline.predict_proba(df)
+
+       probabilities = probabilities.tolist()
+
+       return {'predictions': prediction, 'probabilities': probabilities}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f'This is a server error {str(e)}')
-    
+       raise HTTPException(status_code=500, detail=f'This is a server error {str(e)}')
 
-    
-@app.post('/predict_Sepssis_with_logistic_classifier')
-def predict_Sepssis(sepssis_features3: SepssisFeatures):
+@app.post("/predict_Logistic_Model")
+def predict_Logistic(data: SepssisFeatures):
+    try:
+     df = pd.DataFrame([data.model_dump()])
 
-    try: 
-    
-        df = pd.DataFrame([sepssis_features3.model_dump()])
+     prediction = logistic_pipeline.predict(df)
 
-        prediction3 = pipeline3.predict(df)
+     prediction = int(prediction[0])
 
-        decoded_prediction3 = encoder.inverse_transform([prediction3])[0]
+     prediction = encoder.inverse_transform([prediction])[0]
 
-        return {'prediction': decoded_prediction3}
-    
+     probabilities = logistic_pipeline.predict_proba(df)
+
+     probabilities = probabilities.tolist()
+
+     return {'predictions': prediction, 'probabilities': probabilities}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f'This is a server error {str(e)}')
+       raise HTTPException(status_code=500, detail=f'This is a server error {str(e)}')
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
